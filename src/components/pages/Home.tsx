b010c71846dfcce0789fa4503226216a64cbd263
unknown <Lucasso461@gmail.com>
2026-03-17 22:00:44 -0300

    Excluir quadros

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -2,6 +2,9 @@ import Lottie from "lottie-react"
 import AeroPlane from "../animations/Aero plane.json"
 import {Link} from "react-router-dom"
 import {useState,useEffect} from "react"
+import TrashIcon from "../icons/TrashIcon"
+import Card from "../layouts/Card"
+import type {tipos} from "../types/CardType"
 type Board={
     id:number,
     color:string,
@@ -9,6 +12,20 @@ type Board={
 }
 export default function Home(){
     const [boards,setBoards]=useState<Board[]>([])
+    const [msg,setMsg]=useState("")
+    const [tipo,setTipo]=useState<tipos>("success")
+    async function exclude(id:number){
+        const response=await fetch("http://localhost:3000/deleteBoard",{
+            method:"DELETE",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({board_id:id})
+        })
+        const res=await response.json()
+        setMsg(res.msg)
+        setTipo(res.tipo)
+    }
     useEffect(()=>{
         async function request(){
             const response=await fetch("http://localhost:3000/boards",{
@@ -37,11 +54,15 @@ export default function Home(){
             {boards.map((valor)=>(
                 <div key={valor.id} style={{background:valor.color}} className="w-3/4 h-1/8 my-1 text-white flex items-center justify-around rounded-xl font-semibold text-lg">
                     <div className="w-1/2">{valor.name}</div>
+                    <TrashIcon onClick={()=>exclude(valor.id)}/>
                 </div>
             ))}
             </div>
         )}
             <Link to="/addboard" className="w-1/2 bg-green-800 font-semibold p-3 rounded-xl text-lg flex items-center justify-center absolute right-[5%] bottom-[15%]"><span className="text-4xl mb-2">+ &nbsp;</span>Criar Quadro</Link>
+            {msg&&msg.length>0&&(
+                <Card msg={msg} tipo={tipo}/>
+            )}
         </div>
     )
 }
